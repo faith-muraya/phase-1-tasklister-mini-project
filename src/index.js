38a4aa819671243593e2261d15378ebd89cf90fe
Faith Muraya <111008831+faith-muraya@users.noreply.github.com>
2023-09-27 13:22:47 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let form = document.getElementById('create-task-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  toDoList(e.target['new-task-description'].value);
+  form.reset()
+})
 });
+
+function toDoList(todo) {
+  let li = document.createElement('li')
+  li.textContent = `${todo}`
+  let ul = document.querySelector('ul')
+  ul.appendChild(li)
+  let btn = document.createElement('button')
+  btn.textContent = " X "
+  li.appendChild( btn)
+  btn.addEventListener('click', deleteButton)
+}
+
+function deleteButton(e) {
+  e.target.parentNode.remove();
+}
+
